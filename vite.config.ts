48b961dd93c762c2c5b106d1f5424a9cbe065940
ngmtine <ngmfries@gmail.com>
2023-11-19 16:41:44 +0900

    dev: ビルドディレクトリ変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: process.env.GITHUB_PAGES ? "table_virtuoso_example" : "./",
+    build: {
+        outDir: "./docs",
+    },
 });
